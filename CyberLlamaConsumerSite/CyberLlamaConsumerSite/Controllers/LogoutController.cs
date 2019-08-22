87dc6ddb5a7cde45da31be3548f828831751bc6e
Matthew <218029802@student.uj.ac.za>
2019-08-22 12:27:06 +0200

    add logout function

---
+++ CyberLlamaConsumerSite/CyberLlamaConsumerSite/Controllers/LogoutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CyberLlamaConsumerSite.Controllers
+{
+    public class LogoutController : Controller
+    {
+        // GET: Logout
+        public ActionResult Index()
+        {
+            this.Session["UserSession"] = null;
+            return this.Redirect(Url.Action("Index", "Home"));
+        }
+    }
+}
