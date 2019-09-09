f95bfe57e1df919d3fb990d523611a5c19c5fb73
Peter Jordaan <218047693@student.uj.ac.za>
2019-09-09 11:49:47 +0200

    Embedding the DB
    Adding the DB to the App_Data file in the Service

--- Services/AdministratorService/App_Code/Service.cs
+++ Services/AdministratorService/App_Code/Service.cs
@@ -5,7 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
-using Newtonsoft.Json;
+//using Newtonsoft.Json;
 
 
 // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
