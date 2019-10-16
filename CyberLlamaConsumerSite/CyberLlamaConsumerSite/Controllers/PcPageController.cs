65a1ea006882be7db14e4e794d3ebd63b1d5b7c8
Peter Jordaan <218047693@student.uj.ac.za>
2019-10-16 02:10:36 +0200

    Added the PC Info Page

---
+++ CyberLlamaConsumerSite/CyberLlamaConsumerSite/Controllers/PcPageController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CyberLlamaConsumerSite.Controllers
+{
+    public class PcPageController : Controller
+    {
+        // GET: PcPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void AddToCart(int ID)
+        {
+            CRUDService.ServiceClient sc = new CRUDService.ServiceClient();
+            if(Session["UserID"] != null)
+            {
+                sc.addToPcCart(Convert.ToInt32(Session["UserID"]), ID, 1, 100);
+
+            }
+            else
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+
+        }
+    }
+}
